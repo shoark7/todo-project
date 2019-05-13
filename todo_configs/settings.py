6aed37c3224d2f4891373e28072e6ba13d134c9d
Sunghwan Park <shoark7@gmail.com>
2019-05-13 18:51:46 +0900

    Create todos app model

--- todo_configs/settings.py
+++ todo_configs/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
---
+++ todos/models.py
@@ -0,0 +1,21 @@
+from datetime import date
+from django.db import models
+
+
+LONG_FUTURE = date(3000, 12, 31)
+
+class TodoList(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    importance = models.CharField(
+        choices=tuple((str(n), n) for n in range(1, 7+1))
+    )
+    created_date = models.DateField(auto_now_add=True)
+    expired_date = models.DateField(default=LONG_FUTURE)
+    is_solved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title[:10] + ': '+ self.content[:10]
+
+    def __repr__(self):
+        return self.__str__()
