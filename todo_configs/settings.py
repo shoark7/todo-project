89526ffd226282ac6780a057f8958364a7b4d229
Sunghwan Park <shoark7@gmail.com>
2019-05-18 10:25:04 +0900

    Change template setting

--- todo_configs/settings.py
+++ todo_configs/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join('templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
