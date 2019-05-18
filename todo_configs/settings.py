e15829f0429178565c195549365c5365b8d86c1d
Sunghwan Park <shoark7@gmail.com>
2019-05-18 10:56:33 +0900

    Update settings

--- todo_configs/settings.py
+++ todo_configs/settings.py
@@ -100,7 +100,7 @@ TEMPLATES = [
     },
 ]
 
-STATIC_URL = '/static/'
+STATIC_URL = '/www/static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'www', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
